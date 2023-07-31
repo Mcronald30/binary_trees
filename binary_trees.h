cdd014fb95faa87da97b3df2ed032488588e36de
Ronald <nottinsonstephen@gmail.com>
2023-07-31 11:00:38 -0700

    0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -18,7 +18,23 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
+/**
+ * struct link_s - structure for advanced tasks
+ *
+ * @n: depth of node specified
+ * @node: node of tree to store
+ * @next: next node of the linked list
+ */
+typedef struct link_s
+{
+	size_t n;
+	struct binary_tree_s const *node;
+	struct link_s *next;
+} link_t;
 void binary_tree_print(const binary_tree_t *);
-
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif /* _BINARY_TREES_H_ */
