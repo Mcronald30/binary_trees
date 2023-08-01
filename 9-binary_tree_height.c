7c3bfce467f98cb4c3a4f5533aa84419e44669f7
Ronald <nottinsonstephen@gmail.com>
2023-08-01 10:48:11 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,6 @@
 /**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: root node of the tree
- * Return: If tree is NULL.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
