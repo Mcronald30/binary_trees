5835e9f32fa9fbcd42c55c277e837080566f99c9
Ronald <nottinsonstephen@gmail.com>
2023-08-01 10:39:30 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_h = 0;
-	size_t r_h = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
+	{
 		return (0);
-
-	l_h = binary_tree_height(tree->left);
-	r_h = binary_tree_height(tree->right);
-
-	return (1 + (l_h > r_h ? l_h : r_h));
+	}
+	else
+	{
+		if (tree)
+		{
+			left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((left_height > right_height) ? left_height : right_height);
+	}
 }
