4d7e53952213d056fa0049b360239e527fc693ac
Ronald <nottinsonstephen@gmail.com>
2023-08-02 11:11:41 -0700

    18-binary_tree_uncle.c'

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,14 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *grandpa = node->parent->parent;
-
-	if (grandpa->left == node->parent)
-	{
-		return (grandpa->right);
-	}
-	else
-	{
-		return (grandpa->left);
-	}
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
