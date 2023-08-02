f67fef803f4f0e27b5724d515eaa7b4fda31e93c
Ronald <nottinsonstephen@gmail.com>
2023-08-02 04:38:54 -0700

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,18 +7,19 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves = 0;
+	size_t left_leaves = 0;
+	size_t right_leaves = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	if (tree->left == NULL && tree->right == NULL)
+	else
 	{
-		return (1);
+		left_leaves = binary_tree_leaves(tree->left);
+		right_leaves = binary_tree_leaves(tree->right);
+
+		return ((!left_leaves && !right_leaves) ? leaves + 1 : leaves + 0);
 	}
-
-	size_t left = binary_tree_leaves(tree->left);
-	size_t right = binary_tree_leaves(tree->right);
-
-	return (left + right);
 }
