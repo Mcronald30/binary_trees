195a19e6fa6617e9dc8cd6dc1a376c2c61adfe88
Ronald <nottinsonstephen@gmail.com>
2023-08-03 12:03:08 -0700

    103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,15 +7,15 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *pivot;
+
 	if (tree == NULL || tree->right == NULL)
 	{
 		return (NULL);
 	}
 
-	binary_tree_t *pivot = tree->right;
-
+	pivot = tree->right;
 	tree->right = pivot->left;
-
 	if (pivot->left != NULL)
 	{
 		pivot->left->parent = tree;
